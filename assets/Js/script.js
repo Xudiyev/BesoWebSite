6559cfbe91d7fe87bc0f85197112ea67c7576205
Arkan <arkankhudiyev@gmail.com>
2023-10-06 03:10:40 +0400

    BesoHeaderSection

---
+++ assets/Js/script.js
@@ -0,0 +1,17 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate')
+        }
+        // if want to repeat scroll animation using
+        else {
+            sec.classList.remove('show-animate')
+        }
+    })
+}
